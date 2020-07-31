56f36f4a20b410d8f2fee28da9fdae226d4dd469
matautkus15 <33283615+matautkus15@users.noreply.github.com>
2020-07-31 19:50:24 -0400

    Add files via upload

---
+++ macros/macros/src/main/java/com/project/macros/MacrosApplication.java
@@ -0,0 +1,13 @@
+package com.project.macros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrosApplication.class, args);
+	}
+
+}
---
+++ macros/macros/src/test/java/com/project/macros/MacrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.macros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
