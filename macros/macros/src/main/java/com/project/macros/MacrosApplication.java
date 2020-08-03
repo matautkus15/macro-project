833c4640171c53b8588f174c148fbdf3baba9498
matautkus15 <33283615+matautkus15@users.noreply.github.com>
2020-08-03 18:05:07 -0400

    added a message to the main class

--- macros/macros/src/main/java/com/project/macros/MacrosApplication.java
+++ macros/macros/src/main/java/com/project/macros/MacrosApplication.java
@@ -8,7 +8,12 @@ public class MacrosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MacrosApplication.class, args);
-		//test
+		
+		StringBuilder message = new StringBuilder();
+		message.append("Sup boyo\n" )
+			.append("This is your main class and the code written here will be excuted first when the application is started!\n")
+			.append("Look in the console down below to see this message!");
+		System.out.println(message.toString());
 	}
 
 }
